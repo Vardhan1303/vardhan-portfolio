35df33da396c271aa2ef932781ccdf9a2a0dd94f
Vardhan Mistry <mistryvardhan133@gmail.com>
2026-02-23 19:27:03 +0100

    some change

--- src/App.jsx
+++ src/App.jsx
@@ -51,8 +51,8 @@ function ScrollToTopButton() {
       }}
     >
       <svg
-        width="50"
-        height="50"
+        width="100"
+        height="100"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
